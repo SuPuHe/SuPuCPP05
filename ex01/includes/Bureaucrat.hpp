a17f1dcb14f2b458d0f37e8ca8169309d1808525
SuPuHe <mizin.a.s.2006@gmail.com>
2025-11-27 13:49:01 +0100

    init ex01

---
+++ ex01/includes/Bureaucrat.hpp
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: omizin <omizin@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/27 12:06:55 by omizin            #+#    #+#             */
+/*   Updated: 2025/11/27 13:46:13 by omizin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include "main.hpp"
+
+class Bureaucrat
+{
+	private:
+		const std::string	_name;
+		int					_grade;
+	public:
+		Bureaucrat();
+		Bureaucrat(std::string name, int grade);
+		Bureaucrat(Bureaucrat const &copy);
+		Bureaucrat &operator=(Bureaucrat const &copy);
+		~Bureaucrat();
+
+		std::string		getName()const;
+		int				getGrade()const;
+
+		void			incrementGrade();
+		void			decrementGrade();
+
+		class GradeTooHighException : public std::exception
+		{
+			public:
+				const char *what() const throw();
+		};
+
+		class GradeTooLowException : public std::exception
+		{
+			public:
+				const char *what() const throw();
+		};
+
+};
+
+std::ostream &operator<<(std::ostream &out, const Bureaucrat &b);
