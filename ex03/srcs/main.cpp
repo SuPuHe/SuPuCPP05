be93b4ff7b18c8589da8cf02fd6cf8458c4924b5
SuPuHe <mizin.a.s.2006@gmail.com>
2025-12-04 12:10:53 +0100

    finish main

--- ex03/srcs/main.cpp
+++ ex03/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: omizin <omizin@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/27 13:12:25 by omizin            #+#    #+#             */
-/*   Updated: 2025/12/03 15:02:32 by omizin           ###   ########.fr       */
+/*   Updated: 2025/12/04 12:09:40 by omizin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,61 @@ int main()
 {
 	srand(time(NULL));
 
+	std::cout << "=== Creating forms ===\n" << std::endl;
+
 	Intern a;
-	AForm* rrf;
-	rrf = a.makeForm("robotomy Request", "Bender");
+	AForm* rrf_s;
+	AForm* rrf_r;
+	AForm* rrf_p;
+	Bureaucrat hight ("Hight", 1);
 
-	Bureaucrat b ("as", 1);
-	b.signForm(*rrf);
-	b.executeForm(*rrf);
+	rrf_s = a.makeForm("shrubbery creation", "home");
+	rrf_r = a.makeForm("robotomy Request", "Bender");
+	rrf_p = a.makeForm("presidential pardon", "Bender");
 
-	delete rrf;
+	std::cout << *rrf_s << std::endl;
+	std::cout << *rrf_r << std::endl;
+	std::cout << *rrf_p << std::endl;
 
+	std::cout << "\n=== Trying to execute not signed forms ===" << std::endl;
+	try
+	{
+		hight.executeForm(*rrf_p);
+	}
+	catch (std::exception &e)
+	{
+		std::cout << "Exception caught: " << e.what() << std::endl;
+	}
+	try
+	{
+		hight.executeForm(*rrf_r);
+	}
+	catch (std::exception &e)
+	{
+		std::cout << "Exception caught: " << e.what() << std::endl;
+	}
+
+	hight.signForm(*rrf_s);
+	hight.signForm(*rrf_r);
+	hight.signForm(*rrf_p);
+
+
+	std::cout << "\n=== Execute shrubbery form ===" << std::endl;
+
+	hight.executeForm(*rrf_s);
+
+	std::cout << "\n=== Execute robotomy form 10 times ===" << std::endl;
+
+	for (int i = 0; i < 10; i++)
+		hight.executeForm(*rrf_r);
+
+	std::cout << "\n=== Execute presidential form ===" << std::endl;
+
+	hight.executeForm(*rrf_p);
+
+	delete rrf_s;
+	delete rrf_p;
+	delete rrf_r;
+
+	return 0;
 }
